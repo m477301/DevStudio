73fa4d6c234602437e44783836329db76a79aa81
Matteo McGuinness <matteo.mcguinness@gmail.com>
2024-02-03 23:32:15 +0000

    moved .gitignore@

---
+++ BouncingBall/sketch.js
@@ -0,0 +1,18 @@
+const WINDOW_SIZE = 500;
+var balls = [];
+var numberBalls = 5;
+
+function setup() {
+ createCanvas(WINDOW_SIZE, WINDOW_SIZE);
+  for(let i = 0; i < numberBalls; i++) {
+    ball = new Ball()
+    balls.push(ball)
+  }
+}
+
+function draw() {
+  background(255);
+  for(let i = 0; i < numberBalls; i++) {
+    balls[i].draw();
+  }
+}
